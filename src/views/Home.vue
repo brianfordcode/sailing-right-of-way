cca5188cf59008bb087088c820852febaa46185f
Brian Ford <brianfordcode@gmail.com>
2022-04-29 13:12:02 -0700

    clear default vue templates, put title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Sailing Right of Way</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped>
+
+h1 {
+  text-align: center;
+  margin: 30px;
+}
+
+
+</style>
